0e73eb86641ac81146b8b6c0d34812bbaecaa624
HAYASAKA Ryosuke <hayasaka.ryosuke@gmail.com>
2024-04-01 04:16:54 +0900

    コマンド修正

--- src/checol/__init__.py
+++
@@ -1,3 +0,0 @@
-import .vcs
-import .gpt
-
--- src/checol/__main__.py
+++ src/checol/__main__.py
@@ -1,29 +1,4 @@
-import fire
-import vcs
-import gpt
-
-API_KEY = "API_KEY"
-
-
-def review(branch_name: str, git_path: str = "."):
-    print("CTRL+C or 'exit' to exit.")
-    git = vcs.Git(git_path)
-    claude = gpt.Claude(API_KEY)
-    diff = git.diff(branch_name)
-    message = claude.send(diff)
-    while True:
-        print('AI > ', end='')
-        for line in message.content[0].text.split("\n"):
-            print(line)
-        print('You > ', end='')
-        user_message = input()
-        if user_message == 'exit':
-            break
-        message = claude.send(user_message)
-
-
-def main():
-    fire.Fire({'review': review})
+from .main import main
 
 
 if __name__ == "__main__":
