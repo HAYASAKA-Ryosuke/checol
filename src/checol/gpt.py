08ed68305e77750aee69992dc5eb4c4f28279cf5
HAYASAKA Ryosuke <hayasaka.ryosuke@gmail.com>
2024-04-01 03:19:55 +0900

    first commit

---
+++ src/checol/gpt.py
@@ -0,0 +1,28 @@
+import anthropic
+
+
+class Claude:
+    def __init__(
+        self,
+        api_key,
+        model="claude-3-haiku-20240307",
+        max_tokens=1000,
+        system="このコードをプロの目線でコードレビューしてください"
+    ):
+        self.client = anthropic.Client(api_key=api_key)
+        self.model = model
+        self.max_tokens = max_tokens
+        self.system = system
+        self.messages = []
+
+    def send(self, message):
+        self.messages.append({'role': 'user', 'content': message})
+        result = self.client.messages.create(
+            model=self.model,
+            max_tokens=self.max_tokens,
+            system=self.system,
+            messages=self.messages,
+        )
+        print(f"Anthropic: {result.content}")
+        self.messages.append({'role': result.role, 'content': result.content[0].text})
+        return result
---
+++ src/checol/vcs.py
@@ -0,0 +1,12 @@
+import git
+
+class Git:
+
+    def __init__(self, repo_path: str='.'):
+        self.repo = git.Repo(repo_path)
+
+    def diff(self, branch_name: str, current_branch: str|None=None):
+        if current_branch is None:
+            current_branch = self.repo.active_branch.name
+        return self.repo.git.diff(branch_name, current_branch)
+    
