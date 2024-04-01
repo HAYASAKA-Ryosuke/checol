556685398becda270e392c4bf2a6796e0a7953c5
HAYASAKA Ryosuke <hayasaka.ryosuke@gmail.com>
2024-04-02 02:11:33 +0900

    部分的な差分ではなくファイル全体を表示するようにした

--- src/checol/vcs.py
+++ src/checol/vcs.py
@@ -8,12 +8,13 @@ class Git:
         self.repo = git.Repo(repo_path)
 
     def head_diff(self, staged: bool = False):
+        import pdb;pdb.set_trace()
         if staged:
-            return self.repo.git.diff("HEAD", "--staged")
+            return self.repo.git.diff("HEAD", "--staged", '-W')
         else:
-            return self.repo.git.diff("HEAD")
+            return self.repo.git.diff("HEAD", '-W')
 
     def diff(self, branch_name: str, current_branch: Optional[str] = None):
         if current_branch is None:
             current_branch = self.repo.active_branch.name
-        return self.repo.git.diff(branch_name, current_branch)
+        return self.repo.git.diff(branch_name, current_branch, '-W')
