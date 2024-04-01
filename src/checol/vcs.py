ce75c9058df3ca2b4480199b0bb1b1d3ac73745f
HAYASAKA Ryosuke <hayasaka.ryosuke@gmail.com>
2024-04-01 14:00:51 +0900

    fmt

--- src/checol/vcs.py
+++ src/checol/vcs.py
@@ -1,15 +1,14 @@
 import git
 
-class Git:
 
-    def __init__(self, repo_path: str='.'):
+class Git:
+    def __init__(self, repo_path: str = "."):
         self.repo = git.Repo(repo_path)
 
     def head_diff(self):
-        return self.repo.git.diff('HEAD')
+        return self.repo.git.diff("HEAD")
 
-    def diff(self, branch_name: str, current_branch: str|None=None):
+    def diff(self, branch_name: str, current_branch: str | None = None):
         if current_branch is None:
             current_branch = self.repo.active_branch.name
         return self.repo.git.diff(branch_name, current_branch)
-    
