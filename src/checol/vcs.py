29d6cb746f4db089726953c3f233ee148ea45796
HAYASAKA Ryosuke <hayasaka.ryosuke@gmail.com>
2024-04-01 15:20:13 +0900

    stagedにも対応した

--- src/checol/vcs.py
+++ src/checol/vcs.py
@@ -7,10 +7,13 @@ class Git:
     def __init__(self, repo_path: str = "."):
         self.repo = git.Repo(repo_path)
 
-    def head_diff(self):
-        return self.repo.git.diff("HEAD")
+    def head_diff(self, staged: bool = False):
+        if staged:
+            return self.repo.git.diff("HEAD", "--staged")
+        else:
+            return self.repo.git.diff("HEAD")
 
-    def diff(self, branch_name: str, current_branch: Optional[str]=None):
+    def diff(self, branch_name: str, current_branch: Optional[str] = None):
         if current_branch is None:
             current_branch = self.repo.active_branch.name
         return self.repo.git.diff(branch_name, current_branch)
