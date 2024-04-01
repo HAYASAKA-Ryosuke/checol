04d0440a6bad565823bdba91beb96fe86f7f4cfd
HAYASAKA Ryosuke <hayasaka.ryosuke@gmail.com>
2024-04-01 15:23:30 +0900

    コマンドを見直した

--- src/checol/main.py
+++ src/checol/main.py
@@ -4,20 +4,14 @@ from checol.vcs import Git
 from checol.gpt import Claude
 
 
-def review(git_path: str = ".", diff_or_branch_name: str = "diff"):
-    print("CTRL+C to exit.")
-    git = Git(git_path)
-    claude = Claude(api_key=os.environ.get("ANTHROPIC_API_KEY"))
-    if diff_or_branch_name == "diff":
-        diff = git.head_diff()
-    else:
-        diff = git.diff(diff_or_branch_name)
+def interact_with_claude(git_diff: str):
     print("Description > ", end="")
+    claude = Claude(api_key=os.environ.get("ANTHROPIC_API_KEY"))
     description = input()
     if description:
-        message = claude.send(f"{description}\n\n{diff}")
+        message = claude.send(f"{description}\n\n{git_diff}")
     else:
-        message = claude.send(diff)
+        message = claude.send(git_diff)
     while True:
         print("AI > ", end="")
         for line in message.content[0].text.split("\n"):
@@ -27,11 +21,26 @@ def review(git_path: str = ".", diff_or_branch_name: str = "diff"):
         message = claude.send(user_message)
 
 
+def uncommitted(staged: bool = False):
+    git_path = os.getcwd()
+    git = Git(git_path)
+    uncommitted_diff = git.head_diff(staged=staged)
+    interact_with_claude(uncommitted_diff)
+
+
+def diff(branch: str = None):
+    git_path = os.getcwd()
+    git = Git(git_path)
+    diff = git.diff(branch)
+    interact_with_claude(diff)
+
+
 def main():
     if os.environ.get("ANTHROPIC_API_KEY") is None:
         print("Please set ANTHROPIC_API_KEY environment variable.")
         return
-    fire.Fire({"review": review})
+    print("CTRL+C to exit.")
+    fire.Fire({"uncommitted": uncommitted, "diff": diff})
 
 
 if __name__ == "__main__":
