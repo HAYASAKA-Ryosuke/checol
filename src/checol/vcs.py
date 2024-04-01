a2cb17e8d63f5dfcad630fa294601b1c04e5a1d4
sio <good.saltydog@gmail.com>
2024-04-01 13:51:51 +0900

    fix type hint error

--- src/checol/vcs.py
+++ src/checol/vcs.py
@@ -1,4 +1,7 @@
+from typing import Optional
+
 import git
+
 
 class Git:
 
@@ -8,7 +11,7 @@ class Git:
     def head_diff(self):
         return self.repo.git.diff('HEAD')
 
-    def diff(self, branch_name: str, current_branch: str|None=None):
+    def diff(self, branch_name: str, current_branch: Optional[str]=None):
         if current_branch is None:
             current_branch = self.repo.active_branch.name
         return self.repo.git.diff(branch_name, current_branch)
