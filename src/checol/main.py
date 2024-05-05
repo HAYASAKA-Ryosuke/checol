2b1579b84e17b3539bee730df34a8f91a4c56a9d
HAYASAKA Ryosuke <hayasaka.ryosuke@gmail.com>
2024-05-05 16:38:41 +0900

    add prisma query feature

--- src/checol/main.py
+++ src/checol/main.py
@@ -10,13 +10,13 @@ from checol.vcs import Git
 spinner = Halo(text="Loading", spinner="dots")
 
 
-def generate_response_from_claude(git_diff: str) -> None:
+def generate_response_from_claude(system_message: str, file_text: str) -> None:
     model = os.environ.get("ANTHROPIC_API_MODEL", "claude-3-haiku-20240307")
-    claude = Claude(api_key=os.environ.get("ANTHROPIC_API_KEY"), model=model)
+    claude = Claude(api_key=os.environ.get("ANTHROPIC_API_KEY"), model=model, system=system_message)
 
     description = prompt("Description > ", multiline=True)
 
-    sending_message = f"{description}\n\n{git_diff}" if description else git_diff
+    sending_message = f"{description}\n\n{file_text}" if description else file_text
 
     spinner.start()
     message = claude.send(sending_message)
@@ -38,7 +38,13 @@ def diff(spec: str = "", cached=False):
     if cached:
         spec = f"{spec} --cached"
     diff = git.diff(spec)
-    generate_response_from_claude(diff)
+    generate_response_from_claude("このコード差分を見てプロの目線でコードレビューしてください", diff)
+
+
+def prismaQuery(prisma_schema_file_path: str = ""):
+    with open(prisma_schema_file_path) as f:
+        prisma_schema = f.read()
+    generate_response_from_claude("Prsimaのスキーマファイルです｡要望に応じてSQLを書いてください", prisma_schema)
 
 
 def main():
@@ -47,7 +53,7 @@ def main():
         return
     print("CTRL+C to exit.")
     print("To confirm, type Enter with an empty space.")
-    fire.Fire({"diff": diff})
+    fire.Fire({"diff": diff, "prismaQuery": prismaQuery})
 
 
 if __name__ == "__main__":
